e2859c4692774bb43afe37ab73adf1cb9bac8bac
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-01-30 15:21:20 -0300

    first commit

---
+++ src/main/java/com/example/demo/ApiBibliotecaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBibliotecaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBibliotecaDigitalApplication.class, args);
+	}
+
+}
